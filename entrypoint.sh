55325d5832c6cab9a21ecd4efdd55f5797fc1437
Simon Scurrell <sjscurrell@gmail.com>
2023-05-15 11:42:28 +0100

    Update entrypoint.sh to check state of SINGLE_FILE env variable
    Update entrypoint.sh to use different S3 command depending on state of SINGLE_FILE environment variable. If SINGLE_FILE is set to 'true' then the cp command will be used, otherwise the sync command will be used

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-#Sync files with amazon s3 bucket app
-aws --region $INPUT_AWS_DEFAULT_REGION s3 sync $DIST_LOCATION_CODE s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --no-progress --delete
+if [[ $SINGLE_FILE == 'true' ]]
+  then
+    #Sync file with amazon s3 bucket app
+    echo Synchronising file with S3...
+    S3_CMD=cp
+  else    
+    #Sync folder with amazon s3 bucket app
+    echo Synchronising folder with S3...
+    S3_CMD=sync
+fi
+
+# use AWS cli to do the work
+aws --region $INPUT_AWS_DEFAULT_REGION s3 $S3_CMD $DIST_LOCATION_CODE s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --no-progress --delete
