7dd646ab7cf2e5eeb1950e3974f525ed9c6b99c7
Simon Scurrell <sjscurrell@gmail.com>
2023-05-15 12:29:55 +0100

    corrected object path when uploading a single file

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,13 @@ if [[ $SINGLE_FILE == 'true' ]]
     #Sync file with amazon s3 bucket app
     echo Synchronising file with S3...
     S3_CMD=cp
+    S3_OBJECT_PATH=$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH/$DIST_LOCATION_CODE
   else    
     #Sync folder with amazon s3 bucket app
     echo Synchronising folder with S3...
     S3_CMD=sync
+    S3_OBJECT_PATH=$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH
 fi
 
 # use AWS cli to do the work
-aws --region $INPUT_AWS_DEFAULT_REGION s3 $S3_CMD $DIST_LOCATION_CODE s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --no-progress 
+aws --region $INPUT_AWS_DEFAULT_REGION s3 $S3_CMD $DIST_LOCATION_CODE s3://$S3_OBJECT_PATH --no-progress 
